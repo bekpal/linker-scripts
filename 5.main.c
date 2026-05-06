7ae24ad98ebadea8340d78c252e6b8204ab388a8
bekpal <bekpal@proton.me>
2026-05-06 17:16:48 +0000

    5.1 fn to load data to ram

--- 5.main.c
+++ 5.main.c
@@ -4,3 +4,20 @@ int init_global = 10;
 int foo( int local) {
     return local + unint_global + init_global;
 }
+
+extern char _data_src_start_;
+extern char _data_dst_start_;
+extern char _data_dst_end_;
+
+void _load_data() {
+    /*char *src = _data_src_start_ ; */ // this is wront
+    char *src = &_data_src_start_ ; // & address should be used
+    char *dst = &_data_dst_start_ ;
+    char *dst_end = &_data_dst_end_;
+
+    while (dst != dst_end) {
+        *dst = *src;
+        dst++;
+        src++;
+    }
+}
